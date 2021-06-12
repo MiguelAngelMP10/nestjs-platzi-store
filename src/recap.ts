c53fb7e1b55af9f902383171e70ea0d3350ad67d
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-11 20:03:34 -0500

    repaso ts

---
+++ src/recap.ts
@@ -0,0 +1,16 @@
+const myName = 'miguel';
+
+const myAge = 12;
+
+const suma = (a: number, b: number) => {
+  return a + b;
+};
+
+suma(12, 22);
+
+class Person {
+  constructor(private age: number, private name: string) {}
+  getSummary() {
+    return `I'm ${this.name} and I'm ${this.age}`;
+  }
+}
