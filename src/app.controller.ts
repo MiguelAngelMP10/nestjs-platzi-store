e895f99c5c6ec3bc8c65fc4e9fb03c2b59bf4812
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-07-28 00:35:50 -0500

    Implementando servicios en tu controlador

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,43 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('nuevo') // 👈 Without slashes
-  newEndpoint() {
-    return 'yo soy nuevo';
-  }
-
-  @Get('/ruta/') // 👈 With slashes
-  hello() {
-    return 'con /sas/';
-  }
-
-  @Get('products/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
-
-  @Get('categories/:id/products/:productId')
-  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
-    return `product ${productId} and ${id}`;
-  }
-
-  @Get('api/:id')
-  getProdu(@Param('id') id: string) {
-    return;
-  }
-
-  @Get('products')
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
-  }
-
-  @Get('products/filter')
-  getProductFilter() {
-    return `yo soy un filter`;
-  }
 }
